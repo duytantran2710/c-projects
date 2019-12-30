3b922bfe5ff9f51dd67b26988f806efd2c59dfae
Travis_Tran <duytantran2710@gmail.com>
2019-12-29 18:19:16 -0800

    create user menu

--- bmi_calculator/bmi.c
+++ bmi_calculator/bmi.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define CLRSCR system("@cls || clear");
 
 int main(void) {
 
-	printf("Hello world\n");
+	int unit_choice;
+	char continue_choice;
+
+	CLRSCR
+
+	printf("BMI Calculator\n");
+	printf("---\n");	
+
+	while(TRUE) {
+		printf(" 1. Imperial Unit\n");
+		printf(" 2. Metric Unit\n");
+		printf("\n");
+		printf("Select: ");
+		scanf(" %d", &unit_choice);
+
+		// continue or exit
+		printf("Continue? (please enter y or n) ");
+		scanf(" %c", &continue_choice);
+
+		if(continue_choice == 'n' || continue_choice == 'N')
+			break;
+	};
+
+	//CLRSCR
 	return 0;
 }
