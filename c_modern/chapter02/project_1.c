0dee148ca97faa333020fc7d015d3c18298e90eb
TRAVIS TRAN <duytantran2710@gmail.com>
2020-01-05 19:32:22 -0800

    add template.c to repo for later use

---
+++ c_modern/chapter02/project_1.c
@@ -0,0 +1,23 @@
+/* 
+    Filename:   project_1.c
+    Author:     nerd-oreo (Travis Tran)
+    Created On: Jan 05, 2020
+    Problem:    Write a program that uses "printf" to display the following picture on the screen:
+           *
+          *
+         *
+    *   *
+     * *
+      *
+*/
+#include <stdio.h>
+
+int main(void) {
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+    return 0;
+}
