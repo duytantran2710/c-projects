5ca8798038fb0f763a59e4be7dddebd134a168ba
Travis <duytantran2710@gmail.com>
2020-01-05 22:41:48 -0800

    initial idea

---
+++ sale_tax_calculator/sale_tax.c
@@ -0,0 +1,9 @@
+/*
+    A program to calculate sale tax based on input from the user.
+
+    The user will choose either to enter tax rate or search for tax rate from the 
+    built-in database.
+
+    The database uses U.S states to search for tax rate
+
+*/
