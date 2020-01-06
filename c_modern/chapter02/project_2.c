41304a26466963919e64250d4a2bdbd74331c38a
TRAVIS TRAN <duytantran2710@gmail.com>
2020-01-05 19:51:18 -0800

    initialize project_2.c

---
+++ c_modern/chapter02/project_2.c
@@ -0,0 +1,7 @@
+/* 
+    Filename:   project_2.c
+    Author:     nerd-oreo (Travis Tran)
+    Created On: Jan 05, 2020
+    Problem:    
+        Write a program that computes the volume of a sphere with a 10-meter radius, using the formula v=4/3PIR^3. Write the fraction 4/3 as 4.0f/3.0f. (Try writing it as 4/3. What happen?) Hint: C doesn't have an exponentiation operator, so you'll need to multiply r by itself twice to compute r^3.
+*/
