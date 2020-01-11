8bc618c28e9e234487908e6860c2dd090d993409
Travis <duytantran2710@gmail.com>
2020-01-11 10:43:28 -0800

    initial project structure

---
+++ sale_tax_calculator/main.c
@@ -0,0 +1,15 @@
+/* 
+    Project:    Sale Tax Calculator
+    Filename:   main.c
+    Author:     nerd-oreo (Travis Tran)
+    Created On: Jan 10, 2020
+    Problem:    Calculate sale tax based on user's state/location
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+int main(void) {
+
+    return 0;
+}
--- sale_tax_calculator/sale_tax.c
+++ sale_tax_calculator/sale_tax.c
@@ -1,9 +1,6 @@
-/*
-    A program to calculate sale tax based on input from the user.
-
-    The user will choose either to enter tax rate or search for tax rate from the 
-    built-in database.
-
-    The database uses U.S states to search for tax rate
-
+/* 
+    Filename:   
+    Author:     nerd-oreo (Travis Tran)
+    Created On: 
+    Problem:    
 */
