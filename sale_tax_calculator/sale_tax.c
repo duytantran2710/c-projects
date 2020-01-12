0a923adf3a099992719a90dca1df847c5c673f2a
TRAVIS TRAN <duytantran2710@gmail.com>
2020-01-11 19:48:16 -0800

    add main menu

--- sale_tax_calculator/sale_tax.c
+++ sale_tax_calculator/sale_tax.c
@@ -1,6 +1,85 @@
 /* 
-    Filename:   
+    Project:    Sale Tax Calculator
+    Filename:   main.c
     Author:     nerd-oreo (Travis Tran)
-    Created On: 
-    Problem:    
+    Created On: Jan 10, 2020
+    Problem:    Calculate sale tax based on user's state/location
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "global_vars.h"
+
+#define CLEAR_SCREEN system("@cls||clear")
+#define TRUE    1
+#define FALSE   0
+
+
+/* Prototype */
+// I/O
+void print_banner();
+void print_main_menu();
+void select_state();
+void calculate_sale_tax();
+
+// utilities
+float convert_rate_to_decimal(float rate);
+
+
+int main(void) {
+    
+    print_main_menu();
+    CLEAR_SCREEN;
+    return 0;
+}
+
+/* Print program banner */
+void print_banner() {
+    printf("***************************************\n");
+    printf("*                                     *\n");
+    printf("*         SALE TAX CALCULATOR         *\n");
+    printf("*            nerd-oero 2020           *\n");
+    printf("*                                     *\n");
+    printf("***************************************\n\n");
+}
+
+/* Prompt main menu and return user choice */
+void print_main_menu() {
+    CLEAR_SCREEN;
+    print_banner();
+    printf("1. Set Your State\n2. Calculate Sale Tax\n3. Quit\n\nSelect: ");
+    scanf(" %c", &choice);
+
+    switch(choice) {
+        case '1':
+            select_state(); break;
+        case '2':
+            calculate_sale_tax(); break;
+        case '3':
+            break;
+        default:
+            printf("Invalid choice. Do you want to continue? (y/n): ");
+            scanf(" %c", &choice);
+            if(choice == 'Y' || choice == 'y') {
+                CLEAR_SCREEN;
+                print_main_menu();
+            } else
+                break;
+    }
+
+}
+
+void select_state() {
+    printf("Select State\n");
+    printf("Press a key to continue\n");
+    getchar();
+}
+
+void calculate_sale_tax() {
+    printf("Calculate Sale Tax\n");
+}
+
+/* Utilities */
+float convert_rate_to_decimal(float rate) {
+    return rate/100.0f;
+}
---
+++ sale_tax_calculator/sale_tax.h
@@ -0,0 +1,33 @@
+#ifndef GLOBAL_VARS_H
+#define GLOBAL_VARS_H
+
+#define DB_SIZE 53
+char choice;
+
+typedef struct StateRate {
+    char state[100];
+    float rate;
+} StateRate;
+
+const StateRate STATE_RATE_DB[DB_SIZE] = {
+    {"Alabama", 4.0f},{"Alaska", 0.0f},{"Arizona", 5.6f},
+    {"Arkansas", 6.5f},{"California", 7.25f},{"Colorado", 2.9f},
+    {"Connecticut", 6.35f},{"Delaware", 0.0f},{"District of Columbia", 5.75f},
+    {"Florida", 6.0f},{"Georgia", 4.0f},{"Guam", 4.0f},
+    {"Hawaii", 4.17f},{"Idaho", 6.0f},{"Illinois", 6.25f},
+    {"Indiana", 7.0f},{"Iowa", 6.0f},{"Kansas", 6.5f},
+    {"Kentucky", 6.0f},{"Louisiana", 4.45f},{"Maine", 5.5f},
+    {"Maryland", 6.0f},{"Massachusetts", 6.25f},{"Michigan", 6.0f},
+    {"Minnesota", 6.88f},{"Mississippi", 7.0f},{"Missouri", 4.23f},
+    {"Montana", 0.0f},{"Nebraska", 5.5f},{"Nevada", 6.85f},
+    {"New Hampshire", 0.0f},{"New Jersey", 6.63f},{"New Mexico", 5.13f},
+    {"New York", 4.0f},{"North Carolina", 4.75f},{"North Dakota", 5.0f},
+    {"Ohio", 5.75f},{"Oklahoma", 4.5f},{"Oregon", 0.0f},
+    {"Pennsylvania", 6.0f},{"Puerto Rico", 10.5f},{"Rhode Island", 7.0f},
+    {"South Carolina", 6.0f},{"South Dakota", 4.0f},{"Tennessee", 7.0f},
+    {"Texas", 6.25f},{"Utah", 5.95f},{"Vermont", 6.0f},
+    {"Virginia", 5.3f},{"Washington", 6.5f},{"West Virginia", 6.0f},
+    {"Wisconsin", 5.0f},{"Wyoming", 4.0f}
+};
+
+#endif
