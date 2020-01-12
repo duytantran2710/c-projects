8dc7d485bde97e0fcc98b41a0aea37f989180390
Travis <duytantran2710@gmail.com>
2020-01-12 00:26:37 -0800

    Completed

--- sale_tax_calculator/sale_tax.c
+++ sale_tax_calculator/sale_tax.c
@@ -36,12 +36,14 @@ int main(void) {
 
 /* Print program banner */
 void banner() {
-    printf("***************************************\n");
-    printf("*                                     *\n");
-    printf("*         SALE TAX CALCULATOR         *\n");
-    printf("*            nerd-oero 2020           *\n");
-    printf("*                                     *\n");
-    printf("***************************************\n\n");
+    printf("*************************************************\n");
+    printf("*                                               *\n");
+    printf("*    Project: Sale Tax Calculator               *\n");
+    printf("*    Author:  Travis Tran (nerd-oreo)           *\n");
+    printf("*    Date:    Jan 11, 2020                      *\n");
+    printf("*    Github:  https://github.com/nerd-oreo      *\n");
+    printf("*                                               *\n");
+    printf("*************************************************\n\n");
 }
 
 /* Prompt main menu and return user choice */
@@ -74,8 +76,10 @@ void main_menu() {
 
 void print_user_state_rate() {
     if(strcmp(user_state_rate.state, "NULL") != 0) {
-        printf("Your State: %s - Tax Rate: %.2f%%\n\n", user_state_rate.state, user_state_rate.rate);
-    }    
+        printf("Your State: %s\nSale Tax Rate: %.2f%%\n\n", user_state_rate.state, user_state_rate.rate);
+    } else {
+        printf("No state is selected.\n\n");
+    }
 }
 
 void select_state() {
@@ -86,19 +90,19 @@ void select_state() {
     banner();
     printf("Please select your state:\n");
     for(int i = 1; i < DB_SIZE; i++) {
-        printf("%2d. %15s", i, STATE_RATE_DB[i].state);
+        printf("%2d. %13s", i, STATE_RATE_DB[i].state);
         counter++;
         if(counter > 3) {
             printf("\n");
             counter = 1;
         } else {
-            printf("\t\t");
+            printf("\t");
         }
     }
     printf("\n\nSelect: ");
     scanf(" %d", &state_selected);
 
-    if(state_selected > 0 && state_selected <= 54) {
+    if(state_selected > 0 && state_selected < 54) {
         user_state_rate = STATE_RATE_DB[state_selected];
     } else {
         printf("Invalid choice. Do you want to continue? (y/n): ");
@@ -111,7 +115,39 @@ void select_state() {
 }
 
 void calculate_sale_tax() {
-    
+    float tax_rate = convert_rate_to_decimal(user_state_rate.rate);
+    float sale_amount, tax_amount, total_amount;
+
+    CLEAR_SCREEN;
+    banner();
+    print_user_state_rate();
+
+    printf("Enter sale amount: $ ");
+    scanf(" %f", &sale_amount);
+
+    // calculate tax amount
+    tax_amount = sale_amount * tax_rate;
+
+    // calculate total amount
+    total_amount = sale_amount + tax_amount;
+
+    // print report
+    printf("\n");
+    printf("------- RESULT -----------\n");
+    printf(" Sale amount:\t$%.2f\n", sale_amount);
+    printf(" Tax amount: \t$%.2f\n", tax_amount);
+    printf(" Total amount:\t$%.2f\n", total_amount);
+    printf("--------------------------\n");
+    printf("\n");
+    printf("1. Calculate Again\n2. Back to Main Menu\n");
+    printf("\n");
+    printf("Select: ");
+    scanf(" %c", &choice);
+
+    if(choice == '1')
+        calculate_sale_tax();
+    else
+        main_menu();
 
 }
 
