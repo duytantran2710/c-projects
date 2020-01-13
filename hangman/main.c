ffb77a97bf287f6d048c0f306d9c72376f049f3d
Travis <duytantran2710@gmail.com>
2020-01-12 23:48:50 -0800

    rename hangman.c to main.c - add game banner and main menu

--- hangman/main.c
+++ hangman/main.c
@@ -6,18 +6,69 @@
     Problem:    A classic hangman game
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include "game_variables.h"
 #include "word_list.h"
+
+#define CLRSCR system("@cls||clear")
 
 
 // prototype
 void game_banner();
+void main_menu();
 
 
 int main(void) {
-    printf("size: %zu\n", sizeof(WORD_LIST));
+    main_menu();
+
+    CLRSCR;
+    exit(0);
     return 0;
 }
 
 void game_banner() {
-    
+    printf("###################+-----------------+######################\n");
+    printf("###################| CLASSIC HANGMAN |######################\n");
+    printf("###################+-----------------+######################\n");
+    printf("##                                                        ##\n");
+    printf("##  ||=======+                  @  @  @@  @ @@  @@@@      ##\n");
+    printf("##  ||       |                  @@@@ @@@@ @@  @ @  @      ##\n");
+    printf("##  ||       O                  @  @ @  @ @   @ @@@@      ##\n");
+    printf("##  ||      /|\\                                    @      ##\n");
+    printf("##  ||       |                                  @@@@      ##\n");
+    printf("##  ||      / \\             @ @@ @@   @@  @ @@            ##\n");
+    printf("##  ||                      @@  @  @ @@@@ @@  @           ##\n");
+    printf("##  ||=================     @   @  @ @  @ @   @           ##\n");
+    printf("##                                                        ##\n");
+    printf("############################################################\n");
+    printf("############################################################\n");
+}
+
+void main_menu() {
+    CLRSCR;
+    game_banner();
+
+    if(error == TRUE) {
+        printf("\n");
+        printf("[*] Error: Invalid choice!\n");
+    }
+
+    printf("\n");
+    printf("1. Start Game\n");
+    printf("2. Quit\n");
+    printf("\n");
+    printf("Select: ");
+    scanf(" %c", &choice);
+
+    switch(choice) {
+        case '1':
+            // start game
+            break;
+        case '2':
+            break;
+        default:
+            error = TRUE;
+            main_menu();
+    }
+
 }
