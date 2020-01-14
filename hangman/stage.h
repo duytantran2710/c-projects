151e0b69d97ff122439355fd4de4c4846a5a742b
Travis <duytantran2710@gmail.com>
2020-01-13 22:31:05 -0800

    add all planned stages

--- hangman/stage.h
+++ hangman/stage.h
@@ -3,8 +3,8 @@
 
 int current_stage;
 
-voif set_stage(int current_stage);
-void initial_stage():
+void set_stage(int);
+void initial_stage();
 void first_stage();
 void second_stage();
 void third_stage();
