b4a67dab3ede85ce72853e675c2bdb840dafe12d
Travis <duytantran2710@gmail.com>
2020-01-13 22:03:29 -0800

    add source file for stage

---
+++ hangman/stage.h
@@ -0,0 +1,14 @@
+#ifndef STAGE_H
+#define STAGE_H
+
+int current_stage;
+
+voif set_stage(int current_stage);
+void initial_stage():
+void first_stage();
+void second_stage();
+void third_stage();
+void fourth_stage();
+void fifth_stage();
+
+#endif
